ccca7e257bc1cbb83395df513d25d390a9840cf3
Stas777 <stas@MacBook-Air-Stas.local>
2016-01-18 17:33:42 +0300

    Initial spring rest application with liquibase
    Initial spring rest application with liquibase

---
+++ src/main/java/by/bsu/staspetrovich/Application.java
@@ -0,0 +1,15 @@
+package by.bsu.staspetrovich;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
